2f1ac03d00de0ebedf20830fa49be627200bb03d
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-06-19 12:12:42 -0400

    user can authenticate

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Route } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { axiosWithAuth } from './utils/axioswithAuth'
 
 import Login from "./components/Login";
 import "./styles.scss";
 
+
+const initalLogin = {
+  username: '',
+  password: ''
+}
+
 function App() {
+
+  const [ login, setLogin ] = useState(initalLogin);
+  
+  let history = useHistory();
+
+  const onInputChange = evt => {
+    const name = evt.target.name
+    const value = evt.target.value
+
+    setLogin({
+        ...login,
+        [name]: value 
+    })
+  }
+
+    const authenticate = (e) => {
+      e.preventDefault()
+        axiosWithAuth()
+            .post('/api/login', login)
+            .then(res =>
+                window.localStorage.setItem('token', res.data.payload),
+                history.push('/'))
+            .catch(err => console.log(err))  
+      };
+
   return (
-    <Router>
+    
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={Login}>
+          <Login login={login} onInputChange={onInputChange} authenticate={authenticate} />
+        </Route>
+
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
       </div>
-    </Router>
+    
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,32 @@
 import React from "react";
 
-const Login = () => {
+
+
+const Login = (props) => {
+
+  
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <p>Login Here</p>
+      <form onSubmit={props.authenticate} className='form'>
+        <label>Username</label>
+            <input 
+              name='username'
+              value={props.login.username}
+              onChange={props.onInputChange}/>
+          <label>Password</label>
+            <input 
+                name='password'
+                value={props.login.password}
+                onChange={props.onInputChange}/>
+
+            <button>Submit</button>
+  
+      </form>
     </>
   );
 };
